706a5502ce3c6ce61484355e87f0fa14c84b5bdb
Lavrus89 <ivanlavlin89@gmail.com>
2023-10-30 10:02:36 +0300

    test commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import xlrd
+from tkinter.filedialog import askopenfilename
+
+
+print(dir(tkinter))
+
+# file = askopenfilename()
+# ws = xlrd.open_workbook(file, encoding_override='1251').sheet_by_index(0)
+# print(ws.cell_value(10, 0))
